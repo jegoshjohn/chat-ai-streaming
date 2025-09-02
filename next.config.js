b31decfa1634b9cbb13b36faf9e50effcf2256b9
jegosh <jegoshjohn91@gmail.com>
2025-09-02 12:30:57 -0700

    Update Dockerfile and nextconfig for aws apprunner

--- next.config.js
+++ next.config.js
@@ -8,21 +8,21 @@ const nextConfig = {
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/api/:path*"
-            : "http://backend:8000/api/:path*",
+            : "http://localhost:8000/api/:path*",
       },
       {
         source: "/docs",
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/docs"
-            : "http://backend:8000/docs",
+            : "http://localhost:8000/docs",
       },
       {
         source: "/openapi.json",
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/openapi.json"
-            : "http://backend:8000/openapi.json",
+            : "http://localhost:8000/openapi.json",
       },
     ];
   },
